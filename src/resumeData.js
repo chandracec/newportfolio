5d2e7a61361ff45fa0bd3be6e2641b49da02d5ec
CHANDRAKANT TIWARI <chandrakant91550@gmail.com>
2023-03-10 12:34:47 +0530

    UPDATED

---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"https://i.ibb.co/bR0sZcV/f2f739d1-ad26-421e-b3cb-89c5ace0686c.jpg",
+    "name": "Chandrakant Tiwari",
+    "role": "Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/cktfromdtg/",
+    "roleDescription": " Fresher Looking for entry Role into field of Software Development",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/cktfromdtg",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/chandracec",
+          "className":"fa fa-github"
+        }
+
+      ],
+    "aboutme":"I am CS graduate having done Full Stack Bootcamp with focus on Frontend side current looking for opening position",
+    "address":"Jharkhand , India",
+    "mobileNum":"9123202854",
+
+    "education":[
+      {
+        "UniversityName":" Chaibasa Emgineering College",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2021",
+        
+      },
+      {
+        "UniversityName":"M D I College ",
+        "specialization":"12th",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+         
+      }
+    ],
+    "BootCamp":[
+      {
+        "CompanyName":"Full Stack Web Development - Newton School",
+        "specialization":"ReactJS javascript Bootstrap",
+        "MonthOfLeaving":"April 2022",
+        "YearOfLeaving":"October 2022",
+      },
+     ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"javaScript"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"MaterialUI"
+      },
+      {
+        "skillname":"Firebase"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Youtube Clone",
+        "description":"WebApp to replicate the design of youtube into an API based app that support feed and search facility ",
+        "imgurl":"https://www.freeiconspng.com/uploads/hd-youtube-logo-png-transparent-background-20.png"
+      },
+      {
+        "name":"Realtime-Chat App",
+        "description":"Chat app build using reactjs and Firebase to chat in realtime with authentication via Google Oauth ",
+        "imgurl":"https://cdn.shopify.com/app-store/listing_images/c9308d01d0e596807cd697bab16009a1/icon/CJj2z7GLi_sCEAE=.png"
+      }
+      ],
+  }
+  
+  export default resumeData
